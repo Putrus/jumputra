e2d4a95e86e7ae6540dbc768039aa4c9ca6bef95
Marcin Putra <marcin.putra.0@gmail.com>
2023-06-20 20:50:05 +0200

    Initial project commit

---
+++ Jumputra/Jumputra.cpp
@@ -0,0 +1,12 @@
+﻿// Jumputra.cpp : Defines the entry point for the application.
+//
+
+#include "Jumputra.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
