cd15c348067e9dfe7d45595b578fb7ee0d804133
Marcin Putra <marcin.putra.0@gmail.com>
2023-06-21 21:05:02 +0200

    Fullscreen test

--- Jumputra/Main.cpp
+++ Jumputra/Main.cpp
@@ -1,10 +1,12 @@
 ﻿#include <SFML/Graphics.hpp>
+#include <iostream>
 
 int main()
 {
    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
    sf::CircleShape shape(100.f);
    shape.setFillColor(sf::Color::Green);
+   bool fullscreen = false;
 
    while (window.isOpen())
    {
@@ -13,6 +15,25 @@ int main()
       {
          if (event.type == sf::Event::Closed)
             window.close();
+
+         if (event.type == sf::Event::KeyReleased)
+         {
+            if (event.key.code == sf::Keyboard::F11)
+            {
+               if (fullscreen)
+               {
+                  fullscreen = false;
+                  window.create(sf::VideoMode(200, 200), "SFML works!");
+               }
+               else
+               {
+                  fullscreen = true;
+                  //window.create(sf::VideoMode(400, 400), "SFML works!");
+                  window.create(sf::VideoMode::getDesktopMode(), "SFML works!", sf::Style::Fullscreen);
+               }
+               std::cout << window.getSize().x << " " << window.getSize().y << std::endl;
+            }
+         }
       }
 
       window.clear();
