7a08bb7df9338f6aac4feb066c2b12d186a26f1a
Marcin Putra <marcin@putra.pro>
2024-03-17 23:19:10 +0100

    Another Entity unit tests

--- ut/game/physics/src/EntityTest.cpp
+++ ut/game/physics/src/EntityTest.cpp
@@ -14,4 +14,14 @@ namespace jp::ut::game::physics
       EXPECT_EQ(math::Vector2<float>(), entity.getAcceleration());
       EXPECT_EQ(math::Vector2<float>(), entity.getVelocity());
    }
+
+   TEST(Entity, ArgumentsConstructor)
+   {
+      Entity entity(math::Rect<float>(1.f, 2.f, 3.f, 4.f),
+         math::Vector2<float>(5.f, 6.f), math::Vector2<float>(7.f, 8.f));
+      EXPECT_EQ(math::Vector2<float>(1.f, 2.f), entity.getPosition());
+      EXPECT_EQ(math::Rect<float>(1.f, 2.f, 3.f, 4.f), entity.getRect());
+      EXPECT_EQ(math::Vector2<float>(5.f, 6.f), entity.getAcceleration());
+      EXPECT_EQ(math::Vector2<float>(7.f, 8.f), entity.getVelocity());
+   }
 }
