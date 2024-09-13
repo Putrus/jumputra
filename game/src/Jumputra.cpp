68e20edad05704272ba6177151b6e7bc3fe49eea
Marcin Putra <marcin@putra.pro>
2024-09-13 23:34:44 +0200

    Add changing window size

--- game/src/Jumputra.cpp
+++ game/src/Jumputra.cpp
@@ -33,6 +33,20 @@ namespace jp::game
          {
             mContext.window.close();
          }
+
+         switch (event.key.code)
+         {
+         case sf::Keyboard::Key::F1:
+            mContext.window.setSize(sf::Vector2u(480, 360));
+            break;
+         case sf::Keyboard::Key::F2:
+            mContext.window.setSize(sf::Vector2u(720, 540));
+            break;
+         case sf::Keyboard::Key::F3:
+            mContext.window.setSize(sf::Vector2u(960, 720));
+         default:
+            break;
+         }
       }
    }
 }
